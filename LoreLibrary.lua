2f4b07c556ac58ffe7a22baec5ba7823932d76ad
LanceDH <kiep_out@hotmail.com>
2016-07-23 21:48:00 +0200

    Revert "7.0.02b"
    This reverts commit ab82149e9a33b405efcdc6c0baff4cfea3008f3e.

--- LoreLibrary.lua
+++ LoreLibrary.lua
@@ -1103,7 +1103,7 @@ function _addon:InitFilter(self, level)
 						_addon:UpdateFilteredLoreList();
 						_addon:UpdateBookList();
 					end
-		Lib_UIDropDownMenu_AddButton(info, level)
+		UIDropDownMenu_AddButton(info, level)
 		
 		info.text = UNCHECK_ALL
 		info.func = function()
